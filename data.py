d4934a017646c5da73795399499b5f257d1aa477
andresgreen-byte <ii713504@iteso.mx>
2022-02-14 23:44:31 -0600

    Final commit of project

--- data.py
+++ data.py
@@ -8,3 +8,11 @@ end = datetime.strptime("2022-01-26", '%Y-%m-%d')
 ponderaciones = funct.csv_cleaner('20200131')
 stock_dataYF = funct.yahoo_download(ponderaciones,start,end)
 titulos = funct.pond(ponderaciones, stock_dataYF, inversion_inicial, comision)
+
+qqq = funct.stock('QQQ',start,end)
+qqq = funct.medidas_des(qqq,0,'QQQ')
+arkk = funct.stock('ARKK',start,end)
+arkk = funct.medidas_des(arkk,0,'ARKK')
+
+voo =funct.stock('VOO',start,end)
+voo = funct.medidas_des(voo,0,'S&P 500')
